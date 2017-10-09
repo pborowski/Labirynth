621269025754fa268c8f430096b1b46340170aa4
Pawel-Komputer\Pawel <borowski.pawell@gmail.com>
2017-10-09 15:31:44 +0200

    Dodanie chodzenia i poprawne wczytywanie labiryntu

--- Labirynth/Labirynth/Program.cs
+++ Labirynth/Labirynth/Program.cs
@@ -6,6 +6,10 @@ using System.Linq;
 using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Runtime.InteropServices;
+using System.Drawing;
+
 
 namespace Labirynth
 {
@@ -16,38 +20,72 @@ namespace Labirynth
             SetWindow();
             Console.OutputEncoding = Encoding.UTF8;
             char[,] lab1 = LoadLabirynth();
+            Point playerPoint = getPlayerCoordinates(lab1);
+            DrawLabirynth(playerPoint, lab1);
+            Console.Write("\n"+playerPoint.X + ""+ playerPoint.Y);
+            
             //char c = '\u2181';
             //Console.Write(c);
             //SetPleyer();
 
-            for (int i = 0; i < 30; i++)
-            {
-                for (int j = 0; j < 30; j++)
-                {
-                    Console.Write(lab1[i,j]);
-                }
-                Console.Write("\n");
-            }
-            Console.ReadLine();
-            Console.Write(lab1[0,1]);
-            /*ConsoleKeyInfo keyInfo;
+            ConsoleKeyInfo keyInfo;
             while ((keyInfo = Console.ReadKey(true)).Key != ConsoleKey.Escape)
             {
                 switch (keyInfo.Key)
                 {
                     case ConsoleKey.UpArrow:
+                        if (lab1[playerPoint.X - 1, playerPoint.Y ] != 'X')
+                        {
+                            lab1[playerPoint.X, playerPoint.Y] = ' ';
+                            lab1[playerPoint.X - 1, playerPoint.Y ] = '&';
+                            DrawLabirynth(playerPoint,lab1);
+                            playerPoint = getPlayerCoordinates(lab1);
+                            Console.Write("\n" + playerPoint.X + "" + playerPoint.Y);
+                        }
                         break;
 
                     case ConsoleKey.RightArrow:
+                        if (playerPoint.Y + 1 > 29)
+                        {
+                            Console.Write("\n\n" + "GRATULACJE WYSZEDŁEŚ Z LABIRYNTU!");
+                        }
+                        else
+                        {
+                            if (lab1[playerPoint.X, playerPoint.Y + 1] != 'X')
+                            {
+                                lab1[playerPoint.X, playerPoint.Y] = ' ';
+                                lab1[playerPoint.X, playerPoint.Y + 1] = '&';
+                                DrawLabirynth(playerPoint, lab1);
+                                playerPoint = getPlayerCoordinates(lab1);
+                                Console.Write("\n" + playerPoint.X + "" + playerPoint.Y);
+                            }
+                        }
+                        
                         break;
 
                     case ConsoleKey.DownArrow:
+                        if (lab1[playerPoint.X + 1, playerPoint.Y] != 'X')
+                        {
+                            lab1[playerPoint.X, playerPoint.Y] = ' ';
+                            lab1[playerPoint.X + 1, playerPoint.Y] = '&';
+                            DrawLabirynth(playerPoint, lab1);
+                            playerPoint = getPlayerCoordinates(lab1);
+                            Console.Write("\n" + playerPoint.X + "" + playerPoint.Y);
+                        }
                         break;
 
                     case ConsoleKey.LeftArrow:
+                        if (lab1[playerPoint.X , playerPoint.Y -1] != 'X')
+                        {
+                            lab1[playerPoint.X, playerPoint.Y] = ' ';
+                            lab1[playerPoint.X, playerPoint.Y -1] = '&';
+                            DrawLabirynth(playerPoint, lab1);
+                            playerPoint = getPlayerCoordinates(lab1);
+                            Console.Write("\n" + playerPoint.X + "" + playerPoint.Y);
+                        }
                         break;
                 }
-            }*/
+            }
 
 
         }
@@ -60,8 +98,8 @@ namespace Labirynth
 
         static void SetWindowSize()
         {
-            Console.SetWindowSize(50, 30);
-            Console.SetBufferSize(50, 30);
+           // Console.SetWindowSize(50, 30);
+           // Console.SetBufferSize(50, 30);
         }
 
         static void SetWindow()
@@ -71,9 +109,41 @@ namespace Labirynth
             Console.Title = "Labirynth Game";
         }
 
-        static void SetPlayer()
+        static Point getPlayerCoordinates(char[,] lab1)
         {
-            
+            Point p = new Point();
+            for (int i = 0; i < 30; i++)
+            {
+                for (int j = 0; j < 30; j++)
+                {
+                    Console.Write(lab1[i, j]);
+                    if (lab1[i, j] == '&')
+                    {
+                        p.X = i;
+                        p.Y = j;
+                    }
+                }
+                Console.Write("\n");
+            }
+            return p;
+        }
+
+        static void DrawLabirynth(Point playerPoint, char[,] lab1)
+        {
+            Console.Clear();
+            for (int i = 0; i < 30; i++)
+            {
+                for (int j = 0; j < 30; j++)
+                {
+                    Console.Write(lab1[i, j]);
+                    if (lab1[i, j] == '&')
+                    {
+                        playerPoint.X = i;
+                        playerPoint.Y = j;
+                    }
+                }
+                Console.Write("\n");
+            }
         }
 
         static char[,] LoadLabirynth()
@@ -84,42 +154,58 @@ namespace Labirynth
             using (StreamReader readtext = new StreamReader("C:\\Users\\Pawel\\repozytoria\\LabirynthRepo\\Labirynth\\Labirynth\\labirynth.txt"))
             {
                 string Map = readtext.ReadToEnd();
-                Console.Write(Map);
+                //System.Console.Write(Map[32].ToString());
+                Console.ReadLine();
+                //Console.Write(Map);
                 int a = 0, b = 0;
+                char currentChar;
                 for (int i = 0; i < Map.Length; i++)
                 {
-                    char currentChar;
-                    if (Map[i] == 1)
+                    
+                    switch (Map[i])
                     {
-                        currentChar = '\u2181';
-                    }
-                    else
-                    {
-                        if (Map[i] == 0)
-                        {
+                        case '1': //ściana
+                            currentChar ='X';//'\u2181';
+                            break;
+                        case '0':
                             currentChar = ' ';
-                        }
-                        else
-                        {
-                            currentChar = 'x';
-                        }
+                            break;
+                        case 'X': //gracz
+                            currentChar = '&';
+                            break;
+                        default:
+                            currentChar = '@';
+                            break;
                     }
-                        
-                    if (a <= 30)
+                    if (currentChar != '@')
                     {
-                        if (b <= 30)
+                        if (a < 29)
                         {
-                            LabirynthOne[a, b] = currentChar;
+                            if (b < 30)
+                            {
+                                LabirynthOne[a, b] = currentChar;
+                            }
+                            else
+                            {
+                                b = 0;
+                                LabirynthOne[a, b] = currentChar;
+                                a++;
+
+                            }
                         }
                         else
                         {
-                            b = 0;
-                            a++;
-                            LabirynthOne[a, b] = currentChar;
+                            if (a == 29)
+                            {
+                                if (b < 30)
+                                {
+                                    LabirynthOne[a, b] = currentChar;
+                                }
+                            }
                         }
+                        //LabirynthOne[a, b] = currentChar;
+                        b++;
                     }
-                    LabirynthOne[a, b] = currentChar;
-                    b++;
                 }
             }
                 
